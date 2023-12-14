24a47f01cf23cb086f6e6c3d02d333a239a5f231
Andhika Dwi Khalisyahputra <andhikadwi980@gmail.com>
2023-12-14 14:44:02 +0700

    fix: user exist check

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,6 +1,8 @@
 import {
+  BadRequestException,
   Body,
   Controller,
+  Get,
   Post,
   UsePipes,
   ValidationPipe,
@@ -15,10 +17,25 @@ export class UserController {
   @Post()
   @UsePipes(ValidationPipe)
   async createUser(@Body() body: CreateUserDto) {
+    const existingUser = await this.userService.findUserByEmail(body.email);
+
+    if (existingUser) {
+      throw new BadRequestException('User already exists.');
+    }
+
     const user = await this.userService.createUser(body);
     return {
       message: 'User created successfully.',
       data: user,
+    };
+  }
+
+  @Get()
+  async findAllUsers() {
+    const users = await this.userService.findAllUsers();
+    return {
+      message: 'Users retrieved successfully.',
+      data: users,
     };
   }
 }
